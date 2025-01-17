257d9d432eeb7e2994911a327acfe458dfec4588
tewit001 <25996@srithatpit.ac.th>
2025-01-17 11:28:58 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,35 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>HTMLเบื่องต้น</title>
+  </head>
+  <body>
+      <p>------การแสดงข้อความแบบหัวข้อ------</p>
+      <div>
+          <h1>HTMLเบื่องต้น(h1)</h1>
+      </div>
+      <div>
+        <h2>HTMLเบื่องต้น(h1)</h2>
+        <h3>HTMLเบื่องต้น(h1)</h3>
+        <h4>HTMLเบื่องต้น(h1)</h4>
+        <h5>HTMLเบื่องต้น(h1)</h5>
+        <h6>HTMLเบื่องต้น(h1)</h6>
+      </div>
+      <p>------การการตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+
+      <div>
+          <b>เตวิชญ์ เนตรภักดี</b><br>
+          <i>เตวิชญ์ เนตรภักดี</i><br>
+          <u>เตวิชญ์ เนตรภักดี</u><br>
+      </div>
+      <p></b>-----การ link -----</b><p>
+      <div>
+          <!--link หน้าเดียว-->
+          <a href = "https://www.google.co.th">Google<</a><br>
+      </div>
+
+  </body>
+  </html>
